78ec22695d44f6d55428c9bf8458c8f4953de9d7
abdul0616 <amatin832@gmail.com>
2024-07-15 15:46:10 +0100

    learning git

---
+++ https.mjs
@@ -0,0 +1,26 @@
+// const { send } = require('./send')
+// const { read } = require('./receive')
+import { send } from '../send.mjs';
+import { read } from '../receive.mjs';
+
+
+
+
+function makeRequest(url, data){
+    send(url, data)
+    return read()
+
+    
+}
+
+
+
+
+const responseData = makeRequest('https://www.google.com','data')
+
+console.log(responseData)  
+
+
+
+
+
